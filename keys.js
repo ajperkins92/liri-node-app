c94a7b3cb39f6257dee70dca1be1364f0efe0855
Alex Perkins <alex.jakob.perkins@gmail.com>
2019-10-26 19:08:00 -0700

    Adds keys

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.9de422e8c0eb4fd19507a17044de1771,
+  secret: process.env.a7703e2369494475996f3a5e4946ecc0
 };
