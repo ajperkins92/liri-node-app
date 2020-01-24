a2936424ea1552532e8fb7e579c1838b857a251c
Alex Perkins <alex.jakob.perkins@gmail.com>
2020-01-23 20:32:27 -0800

    whoops

--- liri.js
+++ liri.js
@@ -3,14 +3,22 @@ var Spotify = require("node-spotify-api");
 var keys = require("./keys");
 var axios = require("axios");
 var moment = require("moment");
-var fs = require("fs")
+var fs = require("fs");
+
+// Initializing Spotify API client
+var spotify = new Spotify(keys.spotify);
+
 
 var command = process.argv[2];
 
-if (process.argv.length > 3) {
-    var input = process.argv.slice(3).join(" ");
-    console.log(input);
-}
+// Function which takes in command line arguments and executes correct function accordingly
+var runThis = function (argOne, argTwo) {
+    pick(argOne, argTwo);
+};
+
+// MAIN PROCESS
+// =====================================
+runThis(process.argv[2], process.argv.slice(3).join(" "));
 
 // * `concert-this`
 // * `spotify-this-song`
